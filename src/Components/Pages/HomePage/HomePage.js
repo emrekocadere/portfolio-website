717bb3daa7c5a86b7dd6822b625c7e600e00bfa7
emrekocadere <emrekocadere34@gmail.com>
2024-01-15 22:08:15 +0300

    feat: Add HomePage component
    Added a new React component named HomePage. It includes a CustomHeader and HomeContent components for the main page layout.

---
+++ src/Components/Pages/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CustomHeader from "../../../Components/Header/CustomHeader"
+import HomeContent from "./HomeContent";
+
+export default function HomePage() {
+    return (
+
+        <div>
+            <CustomHeader></CustomHeader>
+            <div style={{ display: "flex", justifyContent: "center", marginTop: "30px" }}>
+                <HomeContent />
+            </div>
+
+        </div>
+
+
+    );
+}
