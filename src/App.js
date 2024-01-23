2c2252f38ea2f53e4975a764633475040607175b
emrekocadere <emrekocadere34@gmail.com>
2024-01-23 21:59:23 +0300

    feat: Add routing to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 import CustomHeader from './Components/Header/CustomHeader';
+import HomePage from './Components/Pages/HomePage/HomePage';
+import AboutmePage from './Components/Pages/AboutMePage/AboutMePage';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 function App() {
     return(
-        <CustomHeader></CustomHeader>
+        <BrowserRouter>
+
+        <Routes>
+        
+        <Route path="/" element={<HomePage />}/>
+        <Route path="aboutme" element={<AboutmePage/>}/>
+   
+        
+        </Routes>
+
+      </BrowserRouter>
     );
 }
 
