57895f3187b03aa928ac5135691df1651151dd72
emrekocadere <emrekocadere34@gmail.com>
2024-01-24 00:14:33 +0300

    style: Improve layout and styling in HomeContent component

--- src/Components/Pages/HomePage/HomeContent.js
+++ src/Components/Pages/HomePage/HomeContent.js
@@ -2,10 +2,12 @@ import React from 'react';
 import BackgroundImage from "../../../Photos/Background.jpg"
 import Link from '@mui/joy/Link';
 import emre from "../../../Photos/emre.png"
+import { Button, Flex } from 'antd';
+import { Radio, Typography } from 'antd';
 import { motion } from "framer-motion"
 export default function HomeContent() {
     return (
-       
+
 
         <div style={{ paddingTop: "35px", backgroundImage: `url(${BackgroundImage})`, backgroundSize: "cover" }}>
             <div style={{ display: "flex" }}>
@@ -16,7 +18,7 @@ export default function HomeContent() {
                             y: 2,
                             scale: 1,
                             rotate: 0,
-                        }}  transition={ {duration: .7 }} >
+                        }} transition={{ duration: .7 }} >
 
                         <h1 style={{ color: "#209761", fontSize: "55px" }}>Salih Emre Kocadere</h1>
                         <h2 style={{ color: "#209761", opacity: ".5", fontSize: "45px", paddingLeft: "280px" }}>Software Engineer</h2>
@@ -26,10 +28,27 @@ export default function HomeContent() {
                     <img src={emre} alt="Avatar" width="300" height="300" style={{ objectFit: "cover", borderRadius: "25%", borderColor: "#209761", borderWidth: "5px", borderStyle: "solid", }} />
                 </div>
             </div>
-            <Link underline="none"
-                variant="solid" level="h3" sx={{ backgroundColor: "#209761", paddingLeft: "100px" }}>About me</Link>
+
+            <div style={{ display: "flex", flexDirection: "column", width: "250px" }}>
+                <Button type="link" block="true" href='/aboutme' style={{ color: "white", backgroundColor: "#209761", paddingLeft: "100px", marginBottom: "30px", fontSize: "20px",height:"40px"}}>
+                    About Me
+                </Button>
+                <Button type="link"  block="true" href='/projects' style={{ color: "white", backgroundColor: "#209761", paddingLeft: "100px", marginBottom: "30px", fontSize: "20px", height: "40px" }}>
+                    Projects
+                </Button>
+                <Button type="link" block="true" href='/skills' style={{ color: "white", backgroundColor: "#209761", paddingLeft: "100px", marginBottom: "30px", fontSize: "20px", height: "40px" }}>
+                    Skills
+                </Button>
+
+            </div>
+
+
+
+
 
         </div>
 
     );
 }
+
+
