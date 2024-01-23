0deb8c08ce83f321857b03df79bde60aae6dc5e0
emrekocadere <emrekocadere34@gmail.com>
2024-01-24 00:15:37 +0300

    style: Enhance styling and content in AboutmeContent component

--- src/Components/Pages/AboutMePage/AboutMeContent.js
+++ src/Components/Pages/AboutMePage/AboutMeContent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Typography from '@mui/joy/Typography';
+import { Radio, Typography } from 'antd';
+const { Paragraph } = Typography;
 
 export default function AboutmeContent() {
     return (
@@ -10,7 +11,7 @@ export default function AboutmeContent() {
             color: "#209761", borderRadius: "30px", width: "1000px"
         }}>
 
-            <p variant="plain"
+            <Paragraph 
                 style={{ color: "#209761", margin: "30px",fontFamily:"Exo2, sans-serif"}}>
                 SMS Blücher,[Not 1] Alman İmparatorluk Donanması'nın son imal edilen zırhlı kruvazörü. Alman istihbaratınca, hatalı olarak İngiliz Invincible sınıfı kruvazörlerine ait olduğu zannedilen özelliklere göre tasarlanan Blücher, önceki zırhlı kruvazörlere göre daha büyük olup, daha fazla ağır silah taşısa da büyüklük ve silah bakımından Birleşik Krallık ile Alman donanmalarında zırhlı kruvazörler yerine getirilen muharebe kruvazörleriyle rekabet edebilecek konumda değildi. Gemi, ismini 1815 yılındaki Waterloo Muharebesi'nde Prusya kuvvetlerine komuta eden Generalfeldmarschall Gebhard Leberecht von Blücher'den almıştı.
 
@@ -24,7 +25,7 @@ export default function AboutmeContent() {
                 26 Mayıs 1906'da Reichstag, Blücher ile beraber Nassau sınıfı zırhlılar için gerekli fonu sağladı. Gemi, önceki zırhlı kruvazörlerden daha büyük ve daha güçlü olacaktı, Blücher daha güçlü yapıdaki tasarımını gizli bir girişim olarak korudu.[4] Gemi, "E" geçici adıyla sipariş edildi. O dönemde rakip olan İngiliz donanmasının zırhlı kruvazörleri, Blücher'in tasarımında etkili oldu. Almanlar, yeni İngiliz gemilerinin altı veya sekiz adet 23 cm'lik (9,2 inç) top taşıyacağını düşünüyordu.[5] Buna tepki olarak, Blücher'in tasarımında on iki adet 21 cm'lik (8,3 inç) ana bataryanın olmasına izin verildi, bu toplar altı adet ikiz taretlerinde ikişer olmak üzere monte edilecekti. Bu, Blücher'in sekiz adet 21 cm'lik top taşıyan Sharnhorst sınıfından daha güçlü ateş gücüne sahip olacağını gösteriyordu.[6]
 
                 Son karardan bir hafta sonra Blücher'in yapımı için izin verildi. Diğer yandan Alman askerî ataşesi İngiliz Invincible sınıfı kruvazörlerin asıl özelliklerini elde etmişti. Aslında HMS Invincible, zırhlılardakiyle aynı olan sekiz adet 30,5 cm'lik (12 inç) top taşıyordu. Yakın zamanda, bu gemilerin muharebe kruvazörü adında yeni bir savaş gemisi sınıfının habercisi olduğu belli olacaktı. Invincible sınıfının detayları gün ışığına çıktığında, Blücher'in tasarımını değiştirmek için artık geç kalınmıştı. İşin içine yeni bir tasarım için gerekli fonun bulunmaması da dâhil olduğunda, gemi için yapılan çalışmalar plana göre devam etti.[7] Böylece Blücher, daha inşası başlamadan modası geçmiş bir gemi haline geldi. Blücher, ilki 1907 yılında sipariş SMS Von der Tann olan yeni kruvazörlerden dahi kalite bakımından geride kaldı.[8] Buna rağmen Blücher, Alman muharebe kruvazörü filosundaki yerini korudu.[5][Not 2] Sonuç olarak Blücher'in Alman hükûmetine toplam maliyeti 28.532.000 Goldmark olmuştu.[9]
-            </p>
+            </Paragraph>
 
         </div>
 
