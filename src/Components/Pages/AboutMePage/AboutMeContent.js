d669a17632f7dbe82fd71a17fa7cf6c411a38e7e
emrekocadere <emrekocadere34@gmail.com>
2024-01-25 21:01:08 +0300

    style: Update About Me page styling

--- src/Components/Pages/AboutMePage/AboutMeContent.js
+++ src/Components/Pages/AboutMePage/AboutMeContent.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Radio, Typography } from 'antd';
+
 const { Paragraph } = Typography;
+
 
 export default function AboutmeContent() {
     return (
 
         <div style={{
             justifyContent: "center", display: "flex",
-            borderStyle: "solid", borderWidth: "2px", borderColor: "#209761",
-            color: "#209761", borderRadius: "30px", width: "1000px"
+            borderStyle: "solid", borderWidth: "2px", borderColor: "#A838D8",
+            color: "#209761", borderRadius: "30px", width: "1000px",
+            backgroundSize:"cover", backgroundColor:"rgba(0, 0, 0, 0.5)"
         }}>
 
-            <Paragraph 
-                style={{ color: "#209761", margin: "30px",fontFamily:"Exo2, sans-serif"}}>
+            <a 
+                style={{ color: "white", margin: "30px",fontFamily:"Source Code Pro', monospace"}}>
                 SMS Blücher,[Not 1] Alman İmparatorluk Donanması'nın son imal edilen zırhlı kruvazörü. Alman istihbaratınca, hatalı olarak İngiliz Invincible sınıfı kruvazörlerine ait olduğu zannedilen özelliklere göre tasarlanan Blücher, önceki zırhlı kruvazörlere göre daha büyük olup, daha fazla ağır silah taşısa da büyüklük ve silah bakımından Birleşik Krallık ile Alman donanmalarında zırhlı kruvazörler yerine getirilen muharebe kruvazörleriyle rekabet edebilecek konumda değildi. Gemi, ismini 1815 yılındaki Waterloo Muharebesi'nde Prusya kuvvetlerine komuta eden Generalfeldmarschall Gebhard Leberecht von Blücher'den almıştı.
 
                 Blücher, 1907 ile 1909 yılları arasında Kiel'deki Kaiserliche Werft Kiel tersanesinde inşa edildi ve ilk seferine 1 Ekim 1909 tarihinde çıktı. I. Dünya Savaşı'nın ilk zamanları da dâhil olmak üzere görev süresinin çoğunu I. Keşif Grubu'na bağlı olarak geçiren Blücher, 1914 yılında Scarborough, Hartlepool ve Whitby ile Yarmouth Baskını'nda yer aldı.
@@ -22,10 +25,10 @@ export default function AboutmeContent() {
                 Tasarımı
                 Alman zırhlı kruvazörleri "Büyük Kruvazör" (Almanca "Große Kreuzer") olarak adlandırılırdı ve birçok farklı görevi yerine getirebilmek için inşa edilirdi. Gemiler, düşman donanmaların keşif hareketlerini engellemekle beraber savaş hattında da muharebe edebilmek üzere tasarlanırdı.[2] En eski zırhlı kruvazör olan Fürst Bismarck, 1900 yılında Çin'de çıkan Boxer Ayaklanması'nı bastırabilmek için aceleye getirilmişti. Sonraki zırhlı kruvazörler -Scharnhorst sınıfı zırhlı kruvazörler hariç- keşif görevinde bulunmak üzere donanmaya hizmet etti.[3]
 
-                26 Mayıs 1906'da Reichstag, Blücher ile beraber Nassau sınıfı zırhlılar için gerekli fonu sağladı. Gemi, önceki zırhlı kruvazörlerden daha büyük ve daha güçlü olacaktı, Blücher daha güçlü yapıdaki tasarımını gizli bir girişim olarak korudu.[4] Gemi, "E" geçici adıyla sipariş edildi. O dönemde rakip olan İngiliz donanmasının zırhlı kruvazörleri, Blücher'in tasarımında etkili oldu. Almanlar, yeni İngiliz gemilerinin altı veya sekiz adet 23 cm'lik (9,2 inç) top taşıyacağını düşünüyordu.[5] Buna tepki olarak, Blücher'in tasarımında on iki adet 21 cm'lik (8,3 inç) ana bataryanın olmasına izin verildi, bu toplar altı adet ikiz taretlerinde ikişer olmak üzere monte edilecekti. Bu, Blücher'in sekiz adet 21 cm'lik top taşıyan Sharnhorst sınıfından daha güçlü ateş gücüne sahip olacağını gösteriyordu.[6]
+               
 
-                Son karardan bir hafta sonra Blücher'in yapımı için izin verildi. Diğer yandan Alman askerî ataşesi İngiliz Invincible sınıfı kruvazörlerin asıl özelliklerini elde etmişti. Aslında HMS Invincible, zırhlılardakiyle aynı olan sekiz adet 30,5 cm'lik (12 inç) top taşıyordu. Yakın zamanda, bu gemilerin muharebe kruvazörü adında yeni bir savaş gemisi sınıfının habercisi olduğu belli olacaktı. Invincible sınıfının detayları gün ışığına çıktığında, Blücher'in tasarımını değiştirmek için artık geç kalınmıştı. İşin içine yeni bir tasarım için gerekli fonun bulunmaması da dâhil olduğunda, gemi için yapılan çalışmalar plana göre devam etti.[7] Böylece Blücher, daha inşası başlamadan modası geçmiş bir gemi haline geldi. Blücher, ilki 1907 yılında sipariş SMS Von der Tann olan yeni kruvazörlerden dahi kalite bakımından geride kaldı.[8] Buna rağmen Blücher, Alman muharebe kruvazörü filosundaki yerini korudu.[5][Not 2] Sonuç olarak Blücher'in Alman hükûmetine toplam maliyeti 28.532.000 Goldmark olmuştu.[9]
-            </Paragraph>
+               
+            </a>
 
         </div>
 
