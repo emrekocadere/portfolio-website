416147a8b85aa309887d8abeda17e08acabce749
emrekocadere <emrekocadere34@gmail.com>
2024-01-26 18:02:33 +0300

    style: update About Me page layout

--- src/Components/Pages/AboutMePage/AboutMePage.js
+++ src/Components/Pages/AboutMePage/AboutMePage.js
@@ -5,7 +5,7 @@ import BackgroundImage from "../../../Photos/CodeWallpaper.avif"
 export default function AboutmePage() {
     return (
         
-        <div  style={{ backgroundImage: `url(${BackgroundImage})`,backgroundSize:"cover"}}>
+        <div  style={{ backgroundImage: `url(${BackgroundImage})`,backgroundSize:"cover",height:"100vh"}}>
             <CustomHeader></CustomHeader>
             <div style={{ display: "flex", justifyContent: "center", marginTop: "30px" }}>
                 <AboutmeContent />
