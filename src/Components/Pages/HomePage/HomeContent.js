00b30904f22d9577aeee8a787dfd4be645d5e834
emrekocadere <emrekocadere34@gmail.com>
2024-01-26 17:58:38 +0300

    style: update homepage layout and styles
    - Adjusted background image and size
    - Modified typography and colors
    - Aligned buttons and sections for improved responsiveness

--- src/Components/Pages/HomePage/HomeContent.js
+++ src/Components/Pages/HomePage/HomeContent.js
@@ -5,11 +5,12 @@ import emre from "../../../Photos/emre.png"
 import { Button, Flex } from 'antd';
 import { Radio, Typography } from 'antd';
 import { motion } from "framer-motion"
+import "../../.."
 export default function HomeContent({ backgroundImage }) {
     return (
 
 
-        <div style={{ paddingTop: "35px", backgroundImage: `url(${BackgroundImage})`, backgroundSize:"cover",width:"100%",height:"100%"}}>
+        <div className="homepage" style={{ paddingTop: "35px", backgroundImage: `url(${BackgroundImage})`, backgroundSize:"cover",height:"85vh"}}>
             <div style={{ display: "flex" }}>
                 <div >
                     <motion.div
@@ -20,32 +21,27 @@ export default function HomeContent({ backgroundImage }) {
                             rotate: 0,
                         }} transition={{ duration: .7 }} >
 
-                        <h1 style={{ color: "#A838D8", fontSize: "55px" }}>Salih Emre Kocadere</h1>
-                        <h2 style={{ color:  "#A838D8", opacity: ".8", fontSize: "45px", paddingLeft: "280px" }}>Software Engineer</h2>
+                        <h1 style={{ color: "#A838D8", fontSize: "55px",fontFamily:" 'Press Start 2P', system-ui; ",fontSize:"35px" }}>Salih Emre Kocadere</h1>
+                        <h2 style={{ color:  "#A838D8", opacity: ".8", fontSize: "30px", paddingLeft: "250px" }}>Software Engineer</h2>
                     </motion.div>
                 </div>
-                <div style={{ display: "flex", justifyContent: "right", paddingLeft: "400px" }}>
+                <div style={{ display: "flex", justifyContent: "right", paddingLeft: "400px" }}>    
                     <img src={emre} alt="Avatar" width="300" height="300" style={{ objectFit: "cover", borderRadius: "100%", borderColor: "#A838D8", borderWidth: "5px", borderStyle: "solid", }} />
                 </div>
             </div>
 
-            <div style={{ display: "flex", flexDirection: "column", width: "250px" }}>
-                <Button type="link" block="true" href='/aboutme' style={{ color: "#A838D8", paddingLeft: "100px", marginBottom: "30px", fontSize: "20px",height:"40px",borderBlockStyle:"solid",borderColor:"#A838D8",borderWidth:"3px"}}>
+            <div style={{ display: "flex", flexDirection: "column", width: "310px", }} className='buttons'>
+                <a  href='/aboutme' style={{ paddingBlock:"10px",color: "#A838D8", marginLeft: "100px",marginBottom: "30px",paddingLeft:"30px", fontSize: "20px",borderStyle:"solid",borderColor:"#A838D8",borderWidth:"3px",fontFamily:" 'Press Start 2P', system-ui; "}}>
                     About Me
-                </Button>
-                <Button type="link"  block="true" href='/projects' style={{ color: "#A838D8", paddingLeft: "100px", marginBottom: "30px", fontSize: "20px", height: "40px",borderBlockStyle:"solid",borderColor:"#A838D8",borderWidth:"3px"}}>
+                </a>
+                <a  href='/projects' style={{ paddingBlock:"10px",color: "#A838D8", marginLeft: "100px",marginBottom: "30px",paddingLeft:"30px", fontSize: "20px",borderStyle:"solid",borderColor:"#A838D8",borderWidth:"3px",fontFamily:" 'Press Start 2P', system-ui; "}}>
                     Projects
-                </Button>
-                <Button type="link" block="true" href='/skills' style={{ color: "#A838D8", paddingLeft: "100px", marginBottom: "30px", fontSize: "20px", height: "40px",borderBlockStyle:"solid",borderColor:"#A838D8",borderWidth:"3px" }}>
+                </a>
+                <a  href='/skills' style={{ paddingBlock:"10px",color: "#A838D8", marginLeft: "100px",paddingLeft:"30px", fontSize: "20px",borderStyle:"solid",borderColor:"#A838D8",borderWidth:"3px",fontFamily:" 'Press Start 2P', system-ui; "}}>
                     Skills
-                </Button>
+                </a>
 
             </div>
-
-
-
-
-
         </div>
 
     );
