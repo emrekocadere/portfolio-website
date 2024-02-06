0028d2b017e148c0ffcd5765bad3e035cb8ec7a5
emrekocadere <emrekocadere34@gmail.com>
2024-02-06 22:39:08 +0300

    Refactor: HomePage component

--- src/Components/Pages/HomePage/HomePage.js
+++ src/Components/Pages/HomePage/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import CustomHeader from "../../../Components/Header/CustomHeader"
 import HomeContent from "./HomeContent";
 import BackgroundImage from "../../../Photos/CodeWallpaper.avif"
 
@@ -7,8 +6,9 @@ export default function HomePage() {
     return (
 
         <div style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: "cover", width: "100vw",height:"100vh" }}>
-        
-            <HomeContent />
+          
+          <HomeContent />
+          
         </div>
 
 
