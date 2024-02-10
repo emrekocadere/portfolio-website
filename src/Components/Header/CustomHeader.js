05b96a70e1a62eb4eda40be407b16856f3ad076d
emrekocadere <emrekocadere34@gmail.com>
2024-02-10 13:13:57 +0300

    refactor: Rearrange layout and integrate components in CustomHeader

--- src/Components/Header/CustomHeader.js
+++ src/Components/Header/CustomHeader.js
@@ -1,18 +1,32 @@
 import { Header } from 'antd/es/layout/layout';
 import React from 'react';
-import NavigationLinks from './NavigationLinks';
+import SocialLinks from './SocialLinks';
 import CustomNavigation from "./CustomNavigation"
 export default function CustomHeader() {
     const [value, setValue] = React.useState('horizontal');
     return (
+     
+            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                <SocialLinks />
 
-        <Header style={{ backgroundColor: '#209761', height: "100px" }}>
-            <div style={{ flexDirection: "row", display: "flex" }}>
-                <NavigationLinks></NavigationLinks>
-                <div style={{ display: "flex", justifyContent: "right", display: "flex", width: "70%" }}>
-                    <CustomNavigation></CustomNavigation>
-                </div>
+                <CustomNavigation></CustomNavigation>
+
             </div>
-        </Header>
+       
+
+
+
     );
 }
+
+
+
+
+{/* <Header style={{ backgroundColor: '#209761', height: "100px" }}>
+<div style={{ flexDirection: "row", display: "flex" }}>
+    <NavigationLinks></NavigationLinks>
+    <div style={{ display: "flex", justifyContent: "right", display: "flex", width: "70%" }}>
+        <CustomNavigation></CustomNavigation>
+    </div>
+</div>
+</Header> */}
