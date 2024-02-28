86102f4c00fcc0c5a521fce5f75ae6031ca73df1
emrekocadere <emrekocadere34@gmail.com>
2024-02-28 20:24:43 +0300

    style: Adjust social media icons in SocialLinks component

--- src/Components/Header/SocialLinks.js
+++ src/Components/Header/SocialLinks.js
@@ -5,21 +5,13 @@ import { GithubOutlined, MailOutlined, LinkedinOutlined } from "@ant-design/icon
 
 function SocialLinks() {
     return ( 
-        <div style={{ paddingLeft: "70px", display: "flex" }}>
+        <div style={{  display: "flex" ,paddingLeft:"8vh", justifyContent: "center", alignItems: "center" }}>
 
-            <div>
-                {/* <Button type="text" href='/' block style={{ color: "white",fontSize:"23px",paddingBottom:"40px" }} >
-                    salih emre kocadere
-                </Button> */}
-
-                <div style={{  display: "flex" }}>
-                <Button type="text" block style={{ color: "#A838D8",fontSize:"23px",paddingBottom:"35px" }}><GithubOutlined /></Button>
-                <Button type="text" block style={{ color: "#A838D8", fontSize:"23px",paddingBottom:"35px"}}><MailOutlined /></Button>
-                <Button type="text" block style={{ color: "#A838D8",fontSize:"23px",paddingBottom:"35px" }}><LinkedinOutlined /></Button>
+                <div style={{  display: "flex"}}>
+                <Button type="text" block style={{ color: "#A838D8",fontSize:"1.9vw" }}><GithubOutlined /></Button>
+                <Button type="text" block style={{ color: "#A838D8",fontSize:"1.9vw" }}><MailOutlined /></Button>
+                <Button type="text" block style={{ color: "#A838D8",fontSize:"1.9vw" }}><LinkedinOutlined /></Button>
                 </div >
-
-            </div>
-
 
         </div>
     
