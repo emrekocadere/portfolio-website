f395dcd7839a87dcb72a79a9ec62f3ef68c9fef9
emrekocadere <emrekocadere34@gmail.com>
2024-02-28 20:25:07 +0300

    style: Adjust styling and structure in CustomNavigation component

--- src/Components/Header/CustomNavigation.js
+++ src/Components/Header/CustomNavigation.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CustomNavigation() {
     return (
-        <div style={{paddingRight:"70px",display:"flex",gap:"35px"}}>
+        <div className="homepage" style={{display:"flex",gap:"35px", fontSize:"1vw", justifyContent: "center", alignItems: "center",paddingRight:"10vh"}}>
             <Link to="/aboutme" style={{color:"#A838D8"}}>About Me</Link>
             <Link  style={{color:"#A838D8"}}>Skills</Link>
             <Link  style={{color:"#A838D8"}}>Projects</Link>
