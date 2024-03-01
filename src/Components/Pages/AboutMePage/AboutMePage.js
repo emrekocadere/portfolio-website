38b13072c6a9adf2fa52fff82d78594cf40d452e
emrekocadere <emrekocadere34@gmail.com>
2024-03-01 23:48:42 +0300

    style: Update styling in AboutmePage component

--- src/Components/Pages/AboutMePage/AboutMePage.js
+++ src/Components/Pages/AboutMePage/AboutMePage.js
@@ -4,12 +4,18 @@ import CustomHeader from '../../Header/CustomHeader';
 import BackgroundImage from "../../../Photos/CodeWallpaper.avif"
 export default function AboutmePage() {
     return (
-        
-        <div  style={{ backgroundImage: `url(${BackgroundImage})`,backgroundSize:"cover",height:"100vh"}}>
-            <CustomHeader></CustomHeader>
-            <div style={{ display: "flex", justifyContent: "center", marginTop: "30px" }}>
-                <AboutmeContent />
+
+        <div style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: "cover", height: "100vh" }}>
+            <div style={{backgroundColor:"rgba(0, 0, 0, 0.4)",height:"100vh"}}>
+                <div style={{ paddingBlock: "5vh" }}>
+                    <CustomHeader />
+                </div>
+
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <AboutmeContent />
+                </div>
             </div>
+
 
         </div>
 
