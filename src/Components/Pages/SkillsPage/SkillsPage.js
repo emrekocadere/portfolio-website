77ef05dfc9f3a39681750ccf5118f6f81b24adcc
emrekocadere <emrekocadere34@gmail.com>
2024-03-08 12:27:44 +0300

    refactor: Extract skill icons rendering to SkillsContent component

--- src/Components/Pages/SkillsPage/SkillsPage.js
+++ src/Components/Pages/SkillsPage/SkillsPage.js
@@ -2,25 +2,17 @@ import React from "react";
 import CustomHeader from "../../Header/CustomHeader"
 import BackgroundImage from "../../../Photos/CodeWallpaper.avif"
 import react from "../../../Photos/react.png"
-import dotnet from "../../../Photos/dotnet.png"
-import docker from "../../../Photos/docker.png"
-import antdesign from "../../../Photos/antdesign.png"
+import SkillsContent from "./SkillsContent";
+
 
 
 export default function SkillsPage() {
     return (
-
-        <div style={{ paddingTop:"10vh",backgroundImage: `url(${BackgroundImage})`, backgroundSize: "cover", width: "100vw",height:"90vh" }}>
-        <div>
-        <img src={react} style={{width:"7vw"}}/>
-           <img src={dotnet} style={{width:"7vw"}}/>
-           <img src={docker} style={{width:"7vw"}}/>
-           <img src={antdesign} style={{width:"7vw"}}/>
+        <div style={{  backgroundImage: `url(${BackgroundImage})`, backgroundSize: "cover", height: "100vh" }}>
+            <div style={{ paddingBlock: "5vh" }}>
+                <CustomHeader />
+            </div>
+            <SkillsContent/>
         </div>
-
-           
-        </div>
-
-
     );
 }
