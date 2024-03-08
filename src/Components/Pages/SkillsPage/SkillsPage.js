85a9bae39705ab986267a4d9f47e3e1319ee45c3
emrekocadere <emrekocadere34@gmail.com>
2024-03-08 14:21:49 +0300

    style: Update styling in SkillsPage component

--- src/Components/Pages/SkillsPage/SkillsPage.js
+++ src/Components/Pages/SkillsPage/SkillsPage.js
@@ -9,10 +9,13 @@ import SkillsContent from "./SkillsContent";
 export default function SkillsPage() {
     return (
         <div style={{  backgroundImage: `url(${BackgroundImage})`, backgroundSize: "cover", height: "100vh" }}>
+            <div style={{backgroundColor:"rgba(0, 0, 0, 0.4)", height: "100vh"}}>
             <div style={{ paddingBlock: "5vh" }}>
                 <CustomHeader />
             </div>
             <SkillsContent/>
+            </div>
+
         </div>
     );
 }
