4fbe7b9f16bab60eab8d67fbedab8e56f2c6f424
emrekocadere <emrekocadere34@gmail.com>
2024-03-08 14:33:37 +0300

    style: Update navigation links styling in CustomNavigation component

--- src/Components/Header/CustomNavigation.js
+++ src/Components/Header/CustomNavigation.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function CustomNavigation() {
     return (
         <div className="homepage" style={{display:"flex",gap:"35px", fontSize:"1vw", justifyContent: "center", alignItems: "center",paddingRight:"10vh"}}>
-            <Link to="/aboutme" style={{color:"#A838D8"}}>About Me</Link>
-            <Link  style={{color:"#A838D8"}}>Skills</Link>
-            <Link  style={{color:"#A838D8"}}>Projects</Link>
+            <Link to="/aboutme" style={{color:"#A838D8",textDecoration:"none"}}>About Me</Link>
+            <Link  style={{color:"#A838D8",textDecoration:"none"}}>Skills</Link>
+            <Link  style={{color:"#A838D8",textDecoration:"none"}}>Projects</Link>
         </div>
     );
 }
