0b13c0ab3b65d708040411eb9e3df0727f2c5602
emrekocadere <emrekocadere34@gmail.com>
2024-03-08 12:26:02 +0300

    style: Improve styling and animation in SocialLinks component

--- src/Components/Header/SocialLinks.js
+++ src/Components/Header/SocialLinks.js
@@ -1,20 +1,30 @@
 import { Button, Flex } from 'antd';
 import { GithubOutlined, MailOutlined, LinkedinOutlined } from "@ant-design/icons"
-
+import { motion } from "framer-motion"
 
 
 function SocialLinks() {
-    return ( 
-        <div style={{  display: "flex" ,paddingLeft:"8vh", justifyContent: "center", alignItems: "center" }}>
+    return (
+        <div style={{ display: "flex", paddingLeft: "8vh", justifyContent: "center", alignItems: "center" }}>
 
-                <div style={{  display: "flex"}}>
-                <Button type="text" block style={{ color: "#A838D8",fontSize:"1.9vw" }}><GithubOutlined /></Button>
-                <Button type="text" block style={{ color: "#A838D8",fontSize:"1.9vw" }}><MailOutlined /></Button>
-                <Button type="text" block style={{ color: "#A838D8",fontSize:"1.9vw" }}><LinkedinOutlined /></Button>
-                </div >
+            <div style={{ display: "flex" }}>
+
+                <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.8 }}>
+                    <Button type="text" block style={{ color: "#A838D8", fontSize: "1.9vw" }}><GithubOutlined /></Button>
+                </motion.div>
+
+                <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.8 }}>
+                    <Button type="text" block style={{ color: "#A838D8", fontSize: "1.9vw" }}><MailOutlined /></Button>
+                </motion.div>
+
+                <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.8 }}>
+                    <Button type="text" block style={{ color: "#A838D8", fontSize: "1.9vw" }}><LinkedinOutlined /></Button>
+                </motion.div>
+
+            </div >
 
         </div>
-    
+
 
     );
 }
