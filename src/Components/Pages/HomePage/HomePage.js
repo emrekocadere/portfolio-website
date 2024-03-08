e78fcaea4c73fa2150e215ea35362d88d7550e48
emrekocadere <emrekocadere34@gmail.com>
2024-03-08 14:20:48 +0300

    style: Update background styling in HomePage component

--- src/Components/Pages/HomePage/HomePage.js
+++ src/Components/Pages/HomePage/HomePage.js
@@ -6,11 +6,13 @@ export default function HomePage() {
     return (
 
         <div style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: "cover", width: "100vw",height:"100vh" }}>
-          
+          <div style={{backgroundColor: "rgba(0, 0, 0, 0.4)"}}>
+
           <div style={{paddingTop:"10vh"}}>
           <HomeContent />
           </div>
         
+          </div>
           
         </div>
 
