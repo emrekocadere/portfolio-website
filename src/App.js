58cbcb4c85b19557de1886c96c2a38585746c8c6
emrekocadere <emrekocadere34@gmail.com>
2024-03-11 15:51:42 +0300

    feat: Add routing for projects page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import CustomHeader from './Components/Header/CustomHeader';
 import HomePage from './Components/Pages/HomePage/HomePage';
 import AboutmePage from './Components/Pages/AboutMePage/AboutMePage';
 import SkillsPage from './Components/Pages/SkillsPage/SkillsPage';
+import ProjectsPage from './Components/Pages/Projects/ProjectsPage';
+
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 function App() {
     return(
@@ -13,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />}/>
         <Route path="/aboutme" element={<AboutmePage/>}/>
         <Route path="/skills" element={<SkillsPage/>}/>
+        <Route path="/projects" element={<ProjectsPage/>}/>
    
         
         </Routes>
