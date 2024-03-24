bedff7a6cf253923f540fff8861eb83db08332ab
emrekocadere <emrekocadere34@gmail.com>
2024-03-24 15:01:26 +0300

    Refactor: ProjectsContent component
    Added a ConfigProvider with custom theme settings for consistency. Styled Card components with custom border and background colors.

--- src/Components/Pages/Projects/ProjectsContent.js
+++ src/Components/Pages/Projects/ProjectsContent.js
@@ -3,12 +3,29 @@ import { ConfigProvider } from 'antd';
 import { Card, Col, Row } from 'antd';
 
 
+
 const ProjectsContent = () => (
 
-  <Row gutter={40} >
 
-    <Col span={8} style={{}}>
-      <Card title="Card title" bordered={false}>
+<div>
+
+<ConfigProvider
+    theme={{
+      token: {
+        // Seed Token
+        colorPrimary: '#00b96b',
+        borderRadius: 2,
+
+        // Alias Token
+        colorBgContainer: '#f6ffed',
+      },
+    }}
+  >
+
+<Row gutter={40} >
+
+    <Col span={8} >
+      <Card title="Card title" bordered={true} style={{borderStyle: "solid", borderWidth: "0.2vw", borderColor: "#A838D8",backgroundColor:"rgba(67, 22, 86, 0.6 )",color:"white"}}>
         Card content
       </Card>
     </Col>
@@ -45,6 +62,11 @@ const ProjectsContent = () => (
     </Col>
 
   </Row>
+  </ConfigProvider>
+
+</div>
+  
+  
 
 );
 export default ProjectsContent;
