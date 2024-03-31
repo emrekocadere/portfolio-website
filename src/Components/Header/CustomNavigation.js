38aadf01d92d9b67c4e91aeac9bfc91a1bad7a18
emrekocadere <emrekocadere34@gmail.com>
2024-03-31 22:37:26 +0300

    Add: motion animations to CustomNavigation component links

--- src/Components/Header/CustomNavigation.js
+++ src/Components/Header/CustomNavigation.js
@@ -3,11 +3,22 @@ import { motion } from "framer-motion"
 
 function CustomNavigation() {
     return (
-        <div className="homepage" style={{display:"flex",gap:"35px", fontSize:"1vw", justifyContent: "center", alignItems: "center",paddingRight:"10vh"}}>
-            <Link to="/" style={{color:"#A838D8",textDecoration:"none"}}>Home</Link>
-            <Link to="/aboutme" style={{color:"#A838D8",textDecoration:"none"}}>About Me</Link>
-            <Link to="/skills" style={{color:"#A838D8",textDecoration:"none"}}>Skills</Link>
-            <Link to="/projects" style={{color:"#A838D8",textDecoration:"none"}}>Projects</Link>
+        <div className="homepage" style={{ display: "flex", gap: "35px", fontSize: "1vw", justifyContent: "center", alignItems: "center", paddingRight: "10vh" }}>
+            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.8 }}>
+                <Link to="/" style={{ color: "#A838D8", textDecoration: "none" }}>Home</Link>
+            </motion.div>
+
+            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.8 }}>
+                <Link to="/aboutme" style={{ color: "#A838D8", textDecoration: "none" }}>About Me</Link>
+            </motion.div>
+            
+            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.8 }}>
+                <Link to="/skills" style={{ color: "#A838D8", textDecoration: "none" }}>Skills</Link>
+            </motion.div>
+
+            <motion.div whileHover={{ scale: 1.3 }} whileTap={{ scale: 0.8 }}>
+                <Link to="/projects" style={{ color: "#A838D8", textDecoration: "none" }}>Projects</Link>
+            </motion.div>
         </div>
     );
 }
